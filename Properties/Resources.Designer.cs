853ddbd4d60d1537eb155af1c050b884e4ea0cb6
Markus Borg <markus.borg@gmail.com>
2015-10-22 14:40:52 +0200

    Cleaned comments

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,14 +61,6 @@ namespace ImpRec.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to &lt;TrackerCase&gt;&lt;ID&gt;33734&lt;/ID&gt;
-        ///&lt;IA&gt;
-        ///** Impact Analysis&quot;,&quot;asadashi&quot;,&quot;2008-05-28&quot;,&quot;05:14:05&quot;,&quot;2008-05-28&quot;,&quot;05:14:05&quot;,&quot;1)  Q: Is the reported problem Safety Critical?
-        ///    A: No. S3964R is an interface free code and cannot be run in a HI controller.
-        ///2a) Q: Describe how the problem shall be solved.
-        ///    A: The present implementation sets the Thread id to 0 in Threadwork function when a hot removal is detected and on Hot
-        ///insert it is recreated.
-        ///       The change done is not to set the thread id to 0 in threadwork fun [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string impactAnalyses {
             get {
@@ -77,13 +69,6 @@ namespace ImpRec.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 20000
-        ///ControlLib: A printed warning has to occur when the oscillation is limited.
-        ///A printed warning has to occur when the selected Max relay amplitude limits the oscillation. The calculated parameters may not get relevant values. And the controller may get worse parameters than before the tuning efforts. INFORMER: Bengt Hansson
-        ///
-        ///20001
-        ///Unfavourable behaviour after power fail.
-        ///&quot;Unfavourable behaviour after power fail. The control loop shall go to backtracking mode and backtrack the I/O-value for one sca [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string issueText {
             get {
@@ -93,24 +78,6 @@ namespace ImpRec.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
-        ///&lt;root&gt;
-        ///&lt;TEST ID=&quot;3bse031209&quot;/&gt;
-        ///&lt;REQ ID=&quot;msm-dof-002&quot;/&gt;
-        ///&lt;UNSPECIFIED ID=&quot;3bse036351&quot;/&gt;
-        ///&lt;REQ ID=&quot;pr-dlv-050&quot;/&gt;
-        ///&lt;UNSPECIFIED ID=&quot;3bse038691&quot;/&gt;
-        ///&lt;REQ ID=&quot;sac-dof-102&quot;/&gt;
-        ///&lt;REQ ID=&quot;pa-fct-503&quot;/&gt;
-        ///&lt;REQ ID=&quot;sac-dof-10&quot;/&gt;
-        ///&lt;UNSPECIFIED ID=&quot;3bse032063&quot;/&gt;
-        ///&lt;HARDWARE ID=&quot;h_s800iomodulebushwlib&quot;/&gt;
-        ///&lt;UNSPECIFIED ID=&quot;3bse049734&quot;/&gt;
-        ///&lt;TEST ID=&quot;3bse025115&quot;/&gt;
-        ///&lt;REQ ID=&quot;ccp-dof-001&quot;/&gt;
-        ///&lt;REQ ID=&quot;hio-dof-0700&quot;/&gt;
-        ///&lt;REQ ID=&quot;ccp-dof-007&quot;/&gt;
-        ///&lt;UNSPECIFIED ID=&quot;3bse032357&quot;/&gt;
-        ///&lt;REQ ID=&quot;sfc-do [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string semanticNetwork {
             get {
@@ -119,19 +86,6 @@ namespace ImpRec.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 3bse030453,DoF Modulebus Protocol Handler
-        ///3bse030454,DoF Modulebus in SafetyModule
-        ///3bse030691,PTTD Global Test
-        ///3bse030775,DoF Modulebus in AC800M
-        ///3bse030898,FTTD Basic Library
-        ///3bse030937,FTTD IOAccess in IOC Framework
-        ///3bse030940,FTTD Config in IOC Framework
-        ///3bse030942,DTD HWIOAccess Config
-        ///3bse031163,FTTD SM Protocol Handler
-        ///3bse031312,Design Test Description CRC
-        ///3bse031350,DTD Controller Configuration Integrity
-        ///3bse031352,DTD Overrun and Latency
-        ///3bse031484,DTD Virtual Machine Test and Configura [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string translation {
             get {
